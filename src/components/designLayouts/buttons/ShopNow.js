d014b25886b1aa122506e0c38b01c3d3a6097f06
ZzelakS <adamsskalezz@gmail.com>
2024-06-20 16:05:04 +0100

    further changes

--- src/components/designLayouts/buttons/ShopNow.js
+++ src/components/designLayouts/buttons/ShopNow.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ShopNow = () => {
   return (
-    <button className="bg-primeColor text-white text-lg font-bodyFont w-[185px] h-[50px] hover:bg-black duration-300 font-bold">
+    <button className="bg-[#010066] rounded-2xl text-white text-lg font-bodyFont w-[185px] h-[50px] hover:bg-black duration-300 font-bold">
       Contact Us
     </button>
   );
--- src/components/home/Footer/FooterBottom.js
+++ src/components/home/Footer/FooterBottom.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { AiOutlineCopyright } from "react-icons/ai";
+import Afri from "./Afri.png"
 
 const FooterBottom = () => {
   return (
     <div className="w-full bg-[#F5F5F3] group">
-      <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
+      <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-19">
         <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
           <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
             <AiOutlineCopyright />
           </span>
-          Copyright 2024 | Total Access Home Healthcare | All Rights Reserved |
+          Copyright 2024 | Afripace International | All Rights Reserved |
           {/* <a href="https://reactbd.com/" target="_blank" rel="noreferrer">
             <span className="ml-1 font-medium group-hover:text-primeColor">
               Powered by ReactBD.com
             </span>
           </a> */}
         </p>
+      </div>
+      <div className="max-w-container flex md:items-center justify-center h-14 w-14 mx-auto border-t-[1px] pt-5 pb-10">
+        <img src={Afri} alt="afri"/>
       </div>
     </div>
   );
